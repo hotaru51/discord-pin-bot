e605e7ff4894174ef0d1f85cfb7fa39cbcd8e884
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-11-17 16:17:19 +0900

    feat: Bot実行部分を実装

--- bot/src/app.py
+++ bot/src/app.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+import logging
+
 import discord
 
+import bot
 
-print('dicord-pin-bot')
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+DISCORD_TOKEN = os.environ.get('PIN_BOT_TOKEN')
+
+if DISCORD_TOKEN is None:
+    logging.error('Environment variable PIN_BOT_TOKEN is undefined.')
+    sys.exit(1)
+
+client = bot.PinBot(intents=discord.Intents.default())
+client.run(token=DISCORD_TOKEN)
