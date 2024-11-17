06b8549db33534e836a22e7b4943adcf1d5d234d
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-11-17 23:23:55 +0900

    feat: PinStateクラスを実装

--- bot/src/bot.py
+++ bot/src/bot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import json
 
 import discord
 
@@ -29,3 +30,71 @@ class PinBot(discord.Client):
         """
 
         logger.info(str(payload))
+class PinState():
+    """メッセージのリアクションイベントからピン留めの状態を取得、操作するクラス
+
+    Attributes:
+        client (PinBot): PinBotインスタンス
+        event (discord.RawReactionActionEvent): リアクション追加/削除時のイベント
+        reaction (str): 追加/削除されたリアクションのemoji名
+        message (Optional[discord.Message]): 対象のメッセージ fetch実行前はNone
+        existing_reactions (list[str]): イベント発生後のメッセージのリアクション一覧 fetch実行前はNone
+    """
+
+    def __init__(self, event: discord.RawReactionActionEvent, client: PinBot):
+        """PinStateのインスタンスを生成
+
+        リアクションのイベントから対象メッセージ、ピン留めの状態を取得する
+
+        Args:
+            event (discord.RawReactionActionEvent): リアクションのイベント
+        """
+        self.client = client
+        self.event = event
+        self.reaction = self.event.emoji.name
+        self.message: discord.Message | None = None
+        self.existing_reactions: list[str] = []
+
+    async def fetch(self):
+        """対象メッセージのデータを取得する
+        """
+
+        channel_id = self.event.channel_id
+        message_id = self.event.message_id
+
+        logger.info('fetch target message. (channel_id: {}, message_id: {})'.format(
+            channel_id, message_id))
+        self.message = await self.client.get_partial_messageable(
+            self.event.channel_id
+        ).fetch_message(self.event.message_id)
+
+        # イベント発生後のリアクションの一覧取得
+        for r in self.message.reactions:
+            if isinstance(r.emoji, discord.PartialEmoji) or isinstance(r.emoji, discord.Emoji):
+                self.existing_reactions.append(r.emoji.name)
+            else:
+                self.existing_reactions.append(r.emoji)
+
+    def is_pinned(self) -> bool:
+        """ピン留め済みか確認する
+
+        Returns:
+            bool: ピン留め済みならTrue(fetch()実行前は状態に関わらずFalse)
+        """
+
+        if self.message is None:
+            return False
+
+        return self.message.pinned
+
+    def __str__(self) -> str:
+        """ステータスを文字列にJSON整形して返す
+        """
+        res = {
+            'event_type': self.event.event_type,
+            'reaction': self.reaction,
+            'is_pinned': self.is_pinned(),
+            'existing_reactions': self.existing_reactions
+        }
+
+        return json.dumps(res)
